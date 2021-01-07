59a4bca670fad423d3999b331e5b16eb5d81a8ec
Kevin William <kevinwilliam1907@gmail.com>
2021-01-07 11:37:16 +0800

    Change heroku domain

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 
   app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://ask-ladybug.herokuapp.com"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Origin", "https://rc4ask.herokuapp.com"); // update to match the domain you will make the request from
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       next();
   });
